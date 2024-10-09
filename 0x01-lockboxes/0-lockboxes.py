2303cce155e9031092bf2c8c0d52ba6704bb11f4
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-09 13:31:46 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" lockboxes """
+
+
+def canUnlockAll(boxes):
+    '''Determines if all the boxes can be opened'''
+    length = len(boxes)
+    closed_boxes = list(range(1, length))
+    for key in boxes[0]:
+        if key in closed_boxes:
+            closed_boxes.remove(key)
+            openBoxes(boxes, key, closed_boxes)
+    return len(closed_boxes) == 0
+
+
+def openBoxes(boxes, key, closed_boxes):
+    '''Opens the boxes'''
+    for new_key in boxes[key]:
+        if new_key in closed_boxes:
+            closed_boxes.remove(new_key)
+            openBoxes(boxes, new_key, closed_boxes)
