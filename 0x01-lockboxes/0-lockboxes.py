4f1c8378b056dedb1326e7a4a697d893cd26eb97
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-09 14:55:28 +0300

    edit final

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,21 +4,20 @@
 
 def canUnlockAll(boxes):
     '''Determines if all the boxes can be opened'''
-    if not boxes or len(boxes) == 0 or not isinstance(boxes, list):
+    if not boxes or len(boxes) == 0 or type(boxes) is not list:
         return False
     length = len(boxes)
-    closed_boxes = list(range(1, length))
+    opened_boxes = {0}  # set
     for key in boxes[0]:
-        if key < length and key in closed_boxes:  # Added key validation
-            closed_boxes.remove(key)
-            openBoxes(boxes, key,
-                      closed_boxes, length)  # Added length parameter
-    return len(closed_boxes) == 0
+        if key not in opened_boxes:
+            opened_boxes.add(key)
+            openBoxes(boxes, key, opened_boxes)
+    return len(opened_boxes) == length
 
 
-def openBoxes(boxes, key, closed_boxes, length):
+def openBoxes(boxes, key, opened_boxes):
     '''Opens the boxes'''
     for new_key in boxes[key]:
-        if new_key < length and new_key in closed_boxes:
-            closed_boxes.remove(new_key)
-            openBoxes(boxes, new_key, closed_boxes, length)
+        if new_key not in opened_boxes:
+            opened_boxes.add(new_key)
+            openBoxes(boxes, new_key, opened_boxes)
