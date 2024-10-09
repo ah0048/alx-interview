40ed8cee0df07132e340ff47d4d665cfabaaf4d7
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-09 15:00:51 +0300

    edit final

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-""" lockboxes """
+""" Lockboxes """
 
 
 def canUnlockAll(boxes):
-    '''Determines if all the boxes can be opened'''
-    if not boxes or len(boxes) == 0 or not isinstance(boxes, list):
-        return False
-    length = len(boxes)
-    closed_boxes = list(range(1, length))
-    for key in boxes[0]:
-        if key in closed_boxes:
-            closed_boxes.remove(key)
-            openBoxes(boxes, key, closed_boxes)
-    return len(closed_boxes) == 0
-
-
-def openBoxes(boxes, key, closed_boxes):
-    '''Opens the boxes'''
-    for new_key in boxes[key]:
-        if new_key in closed_boxes:
-            closed_boxes.remove(new_key)
-            openBoxes(boxes, new_key, closed_boxes)
+    """ Determines if all boxes can be opened """
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
