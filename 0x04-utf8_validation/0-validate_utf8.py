e99944592078fe85c4c1a1ae77194f7ab4556b6f
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-31 22:36:05 +0300

    edit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,9 +4,23 @@
 
 def validUTF8(data):
     '''validate if data is UTF-8 encoded'''
-    valid = True
+    remaining_bytes = 0
     for byte in data:
-        if byte > 255:
-            valid = False
-            break
-    return valid
+        byte = byte & 0b11111111
+        if remaining_bytes == 0:
+            if byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            elif byte >> 7 == 0b0:
+                continue
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            remaining_bytes -= 1
+
+    return remaining_bytes == 0
