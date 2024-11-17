1282fc23a2a46271a9b0f6153afc23a4d94cdf3c
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-17 23:05:46 +0200

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,21 +3,21 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, async function (error, response, body) {
-    if (error) {
-        console.error('error:', error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-        await new Promise((resolve, reject) => {
-            request(character, function (error, response, body) {
-            if (error) {
-                console.error('error:', error);
-            } else {
-                console.log(JSON.parse(body).name);
-            }
-            resolve();
-            });
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      await new Promise((resolve, reject) => {
+        request(character, function (error, response, body) {
+          if (error) {
+            console.error('error:', error);
+          } else {
+            console.log(JSON.parse(body).name);
+          }
+          resolve();
         });
-        }
+      });
     }
-    });
+  }
+});
