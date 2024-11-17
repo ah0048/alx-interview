283b55676d30ef96c7c0ed5b2aa7ae5a3ae09f96
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-17 23:04:06 +0200

    0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, async function (error, response, body) {
+    if (error) {
+        console.error('error:', error);
+    } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+        await new Promise((resolve, reject) => {
+            request(character, function (error, response, body) {
+            if (error) {
+                console.error('error:', error);
+            } else {
+                console.log(JSON.parse(body).name);
+            }
+            resolve();
+            });
+        });
+        }
+    }
+    });
